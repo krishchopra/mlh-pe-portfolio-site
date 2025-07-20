e319e85dd2bc2b3b7dc5b1d928e8fff69b3ee1e4
Krish Chopra <krish.chopra23@gmail.com>
2025-07-19 23:23:11 -0400

    minor tweak

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,106 +3,115 @@ import os
 import re
 
 # set testing environment
-os.environ['TESTING'] = 'true'
+os.environ["TESTING"] = "true"
 
 from app import app
 
+
 class AppTestCase(unittest.TestCase):
-  def setUp(self):
-    self.client = app.test_client()
-  
-  """
+    def setUp(self):
+        self.client = app.test_client()
+
+    """
   Tests the home page:
   1. Checks the title in the HTML response
   2. Checks the active navbar item is "Home"
   3. Checks the about section is present
   """
-  def test_home(self):
-    response = self.client.get('/')
-    assert response.status_code == 200
-    
-    # test the right title is rendered
-    html = response.get_data(as_text=True)
-    assert re.search(r'<title>(?!<\/title>)*[\w\s|]*Krish Chopra<\/title>', html)
 
-    # test the home navbar item is active
-    assert re.search(r'<a[^>]*\sclass="nav-link active"[^>]*>(?!<\/a>)[\w\s<!->]*Home[\s]*<\/a>', html)
+    def test_home(self):
+        response = self.client.get("/")
+        assert response.status_code == 200
 
-    # test the about content is present
-    assert re.search(r'about me', html, re.IGNORECASE)
+        # test the right title is rendered
+        html = response.get_data(as_text=True)
+        assert re.search(r"<title>(?!<\/title>)*[\w\s|]*Krish Chopra<\/title>", html)
 
-  """
+        # test the home navbar item is active
+        assert re.search(
+            r'<a[^>]*\sclass="nav-link active"[^>]*>(?!<\/a>)[\w\s<!->]*Home[\s]*<\/a>',
+            html,
+        )
+
+        # test the about content is present
+        assert re.search(r"about me", html, re.IGNORECASE)
+
+    """
   Tests the timeline API endpoint:
   1. Checks the GET request, assert 200 response and 0 posts initially.
   2. Test POST request to create a new timeline post.
   3. Check that the timeline post is created and returned in the next GET response.
   """
-  def test_timeline(self):
-    # check initial GET request
-    response = self.client.get('/api/timeline_post')
-    assert response.status_code == 200
-    assert response.is_json
 
-    json = response.get_json()
-    assert "timeline_posts" in json
-    assert len(json["timeline_posts"]) == 0
+    def test_timeline(self):
+        # check initial GET request
+        response = self.client.get("/api/timeline_post")
+        assert response.status_code == 200
+        assert response.is_json
 
-    # create a new timeline post
-    response = self.client.post('/api/timeline_post', data={
-      "name": "John Doe",
-      "email": "john@example.com",
-      "content": "Hello world, I'm John!"
-    })
-    assert response.is_json
+        json = response.get_json()
+        assert "timeline_posts" in json
+        assert len(json["timeline_posts"]) == 0
 
-    # check the response from POST request
-    json = response.get_json()
-    assert json["name"] == "John Doe"
-    assert json["email"] == "john@example.com"
-    assert json["content"] == "Hello world, I'm John!"
-    
-    # check GET request again to see the new post
-    response = self.client.get('/api/timeline_post')
-    assert response.status_code == 200
-    assert response.is_json
+        # create a new timeline post
+        response = self.client.post(
+            "/api/timeline_post",
+            data={
+                "name": "John Doe",
+                "email": "john@example.com",
+                "content": "Hello world, I'm John!",
+            },
+        )
+        assert response.is_json
 
-    json = response.get_json()
-    assert "timeline_posts" in json
-    assert len(json["timeline_posts"]) == 1
+        # check the response from POST request
+        json = response.get_json()
+        assert json["name"] == "John Doe"
+        assert json["email"] == "john@example.com"
+        assert json["content"] == "Hello world, I'm John!"
 
-    # note: cannot test HTML results without executing Node.js code
-    # as timeline posts are rendered on the client
+        # check GET request again to see the new post
+        response = self.client.get("/api/timeline_post")
+        assert response.status_code == 200
+        assert response.is_json
 
-  """
+        json = response.get_json()
+        assert "timeline_posts" in json
+        assert len(json["timeline_posts"]) == 1
+
+        # note: cannot test HTML results without executing Node.js code
+        # as timeline posts are rendered on the client
+
+    """
   Tests the timeline API endpoint for malformed requests:
   1. POST request with missing name
   2. POST request with missing content
   3. POST request with missing email
   """
-  def test_malformed_timeline_post(self):
-    # POST missing name
-    response = self.client.post('/api/timeline_post', data={
-      "email": "john@example.com",
-      "content": "Hello world, I'm John!"
-    })
-    assert response.status_code == 400
-    html = response.get_data(as_text=True)
-    assert "Invalid name" in html
 
-    # POST missing content
-    response = self.client.post('/api/timeline_post', data={
-      "name": "John Doe",
-      "email": "john@example.com"
-    })
-    assert response.status_code == 400
-    html = response.get_data(as_text=True)
-    assert "Invalid content" in html
+    def test_malformed_timeline_post(self):
+        # POST missing name
+        response = self.client.post(
+            "/api/timeline_post",
+            data={"email": "john@example.com", "content": "Hello world, I'm John!"},
+        )
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid name" in html
 
-    # POST missing email
-    response = self.client.post('/api/timeline_post', data={
-      "name": "John Doe",
-      "content": "Hello world, I'm John!"
-    })
-    assert response.status_code == 400
-    html = response.get_data(as_text=True)
-    assert "Invalid email" in html
+        # POST missing content
+        response = self.client.post(
+            "/api/timeline_post", data={"name": "John Doe", "email": "john@example.com"}
+        )
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid content" in html
+
+        # POST missing email
+        response = self.client.post(
+            "/api/timeline_post",
+            data={"name": "John Doe", "content": "Hello world, I'm John!"},
+        )
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid email" in html
--- tests/test_db.py
+++ tests/test_db.py
@@ -7,36 +7,41 @@ MODELS = [TimelinePost]
 # use in-memory SQLite database for tests
 test_db = SqliteDatabase(":memory:")
 
+
 class TestTimelinePost(unittest.TestCase):
-  def setUp(self):
-    # bind model to test db
-    # no need to recursively bind refs/backrefs, all models are included
-    test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
-    test_db.connect()
-    test_db.create_tables(MODELS)
-  
-  def tearDown(self):
-    # not necessary since it's in memory, but good practice
-    test_db.drop_tables(MODELS)
+    def setUp(self):
+        # bind model to test db
+        # no need to recursively bind refs/backrefs, all models are included
+        test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+        test_db.connect()
+        test_db.create_tables(MODELS)
 
-    # close db connection
-    test_db.close()
+    def tearDown(self):
+        # not necessary since it's in memory, but good practice
+        test_db.drop_tables(MODELS)
 
-  def test_timeline_post(self):
-    # create posts
-    first_post = TimelinePost.create(name="John Doe", email="john@example.com", content="Hello world, I'm John!")
-    assert first_post.id == 1
-    second_post = TimelinePost.create(name="Jane Doe", email="jane@example.com", content="Hello world, I'm Jane!")
-    assert second_post.id == 2
+        # close db connection
+        test_db.close()
 
-    # get posts
-    posts = TimelinePost.select()
-    assert len(posts) == 2
+    def test_timeline_post(self):
+        # create posts
+        first_post = TimelinePost.create(
+            name="John Doe", email="john@example.com", content="Hello world, I'm John!"
+        )
+        assert first_post.id == 1
+        second_post = TimelinePost.create(
+            name="Jane Doe", email="jane@example.com", content="Hello world, I'm Jane!"
+        )
+        assert second_post.id == 2
 
-    johns_post = TimelinePost.get(TimelinePost.name == "John Doe")
-    assert johns_post.email == "john@example.com"
-    assert johns_post.content == "Hello world, I'm John!"
+        # get posts
+        posts = TimelinePost.select()
+        assert len(posts) == 2
 
-    janes_post = TimelinePost.get(TimelinePost.name == "Jane Doe")
-    assert janes_post.email == "jane@example.com"
-    assert janes_post.content == "Hello world, I'm Jane!"
+        johns_post = TimelinePost.get(TimelinePost.name == "John Doe")
+        assert johns_post.email == "john@example.com"
+        assert johns_post.content == "Hello world, I'm John!"
+
+        janes_post = TimelinePost.get(TimelinePost.name == "Jane Doe")
+        assert janes_post.email == "jane@example.com"
+        assert janes_post.content == "Hello world, I'm Jane!"
